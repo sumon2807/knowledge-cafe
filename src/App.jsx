a9815fc17f5664076ea889020db59b37391d2344
sumon2807 <sharifulbd911@gmail.com>
2023-03-31 20:09:14 +0600

    calculate total reading time and show the data into side card

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from './components/Card/Card';
 import Header from './components/Header/Header';
 import SideCard from './components/SideCard/SideCard';
 
 const App = () => {
+  const [readTime, setReadTime]=useState([0]);
  const handleReadTime=(time)=>{
-  console.log(time)
+  const previousTime=JSON.parse(localStorage.getItem('readTime'));
+  if(previousTime){
+    const totalTime=previousTime + time;
+    console.log(totalTime);
+    localStorage.setItem('readTime', totalTime);
+    setReadTime(totalTime);
+  }
+  else{
+    localStorage.setItem('readTime', time);
+    setReadTime(time);
+  }
  }
   return (
     <div>
@@ -14,8 +25,8 @@ const App = () => {
           <div className="card-container">
             <Card handleReadTime={handleReadTime}></Card>
           </div>
-          <div className="side-card">
-            <SideCard></SideCard>
+          <div className="side-card ">
+            <SideCard readTime={readTime}></SideCard>
           </div>
         </div>
     </div>
--- src/components/SideCard/SideCard.jsx
+++ src/components/SideCard/SideCard.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 
-const SideCard = () => {
+const SideCard = ({readTime}) => {
+    
     return (
-        <div className='mx-8 p-8 border-2 h-full bg-slate-300'>
-            <div className="calculate-total-time border-2 p-4 mb-4">
-                <h1 className='font-bold'>Spent time on read : 177 min</h1>
-            </div>
-            <div className="calculate-total-time border-2 p-4">
-                <h1 className='font-bold'>Bookmarked Blogs : 8</h1>
+        <div className='mx-8 p-8 border-2 h-full bg-slate-300 relative'>
+            <div className='sticky top-28'>
+                <h1 className='font-bold border-2 p-4 mb-4'>Spent time on read : {readTime}</h1>
+                <h1 className='font-bold border-2 p-4'>Bookmarked Blogs : 8</h1>
             </div>
         </div>
     );
